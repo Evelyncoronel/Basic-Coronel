4d5872b096db78827e03d5bb9346bf9d74be33f2
malbec <evelyn.ec.dc@gmail.com>
2022-07-21 18:18:35 -0300

    creacion de carpeta components - NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+function App() {
+    return (         
+        <>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="Basic" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                         <li><a href="inicio.html">Inicio</a></li>
+                         <li><a href="productos.html">Productos</a></li>
+                         <li><a href="contacto.html">Contacto</a></li>
+                    </ul>
+                </div>
+            </nav>
+                <h2> Las ofertas de la semana </h2>
+        </>
+            );
+}
+
+export default App;
